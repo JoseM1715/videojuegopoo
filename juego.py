d89b9320c4397e6ed2f581ac5149798f9fca6a11
JoseAngel <martinez1215sanchez@gmail.com>
2025-04-06 23:10:43 -0500

    El método de recibir_daño su funcion es quitar vida al personaje cuando sea atacado.

--- juego.py
+++ juego.py
@@ -3,15 +3,27 @@ import random
 
 
 
+
 class Personaje:
      def __init__(self, nombre, vida):
          self.nombre = nombre
          self.vida = vida
          self.vivo = True
-     def recibir_daño(self, nombre , cantidad):
-         self.vida -= cantidad
-           print(f"{self.nombre} recibe {cantidad} de daño. vida restante: {self.vida}")
-           if self.vida <= 0:
-             self.vivo = False
-             print(f"{self.nombre} ha sido derrotado.)
+
+     def recibir_daño(self, cantidad):
+             self.vida -= cantidad
+             print(f"{self.nombre} recibe {cantidad} de daño. vida restante: {self.vida}")
+             if self.vida <= 0:
+                 self.vida = 0
+                 self.vivo = False
+                 print(f"{self.nombre} ha sido derrotado.")
+             else:
+                 print(f"{self.nombre} recibió {cantidad} de daño. su vida restante. {self.vida}")
+
+class Jugador :
+     def __init__(self, nombre):
+         super().__init__(nombre, vida=100)
+         self.puntuacion = 0
+         self.arma = None
+
 
