4b84f30ef3cb22b2bae9d68ea5a6d9c4609e20c9
JoseAngel <martinez1215sanchez@gmail.com>
2025-04-07 00:01:57 -0500

    la clase jugador que modela al personaje principal que es controlado por el jugador y también permite atacar a enemigos, recoger armas y llevar un sistema de puntuacion.

--- juego.py
+++ juego.py
@@ -2,28 +2,32 @@ from abc import  ABC, abstractmethod
 import random
 
 
-
-
 class Personaje:
      def __init__(self, nombre, vida):
          self.nombre = nombre
          self.vida = vida
          self.vivo = True
 
-     def recibir_daño(self, cantidad):
+     def recibir_golpe(self, cantidad):
              self.vida -= cantidad
              print(f"{self.nombre} recibe {cantidad} de daño. vida restante: {self.vida}")
              if self.vida <= 0:
-                 self.vida = 0
                  self.vivo = False
                  print(f"{self.nombre} ha sido derrotado.")
-             else:
-                 print(f"{self.nombre} recibió {cantidad} de daño. su vida restante. {self.vida}")
+
 
 class Jugador :
      def __init__(self, nombre):
          super().__init__(nombre, vida=100)
          self.puntuacion = 0
          self.arma = None
+     def atacar(self, enemigo):
+         if self.arma and self.arma.esta_disponble():
+             poder = self.arma.usar()
+             print(f"{self.nombre} ata con {self.arma.nombre} y causa {poder} de daño.")
+             enemigo.recibir_golpe(poder)
+             if not  enemigo.vivo:
+                 self.puntuacion += 10
+                 print(f"{self.nombre} derrota a {enemigo.nombre} gana 10 puntos. puntacion totla:{self.puntuacion} ")
 
 
